692c3854a22e37595c8ea9a943543cf5b3a32243
khNishad <kkhnishad@gmail.com>
2020-04-14 13:08:51 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request  = require('request');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+
+var city = 'Tangail';
+
+
+
+
+app.get('/',function (req,res) {
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
+
+    request(url , function (error , respone , body) {
+     var  weather_json =   JSON.parse(body);
+    // console.log(weather_json);
+
+     var data = {
+         city: city ,
+         temp: Math.round(weather_json.main.temp) ,
+         icon: weather_json.weather[0].icon,
+         des : weather_json.weather[0].description,
+         humi: weather_json.main.humidity,
+
+     }
+        var data = { data: data }
+        res.render('weather.ejs', data);
+    });
+   
+});
+
+
+app.post('/weather', (req,res)=>{
+    var city  = req.body.city2
+    res.redirect('/weather2/'+city);
+})
+
+app.get('/weather2/:city', function (req, res) {
+    var city  = req.params.city;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
+
+    request(url, function (error, respone, body) {
+        var weather_json = JSON.parse(body);
+       // console.log(weather_json.message);
+        
+        if (!weather_json.message)
+        {
+
+            var data = {
+                city: city,
+                temp: Math.round(weather_json.main.temp),
+                icon: weather_json.weather[0].icon,
+                des: weather_json.weather[0].description,
+                humi: weather_json.main.humidity,
+
+            }
+            var data = { data: data }
+            res.render('weather2.ejs', data);
+        }else{
+            res.send('City not found');
+        }
+
+
+    });
+
+});
+
+
+
+
+app.listen(3000);
+console.log('listining');
