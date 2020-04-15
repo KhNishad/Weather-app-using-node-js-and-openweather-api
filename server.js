4f013e9b8fe457846418f4b909c7e84311d625cf
khNishad <kkhnishad@gmail.com>
2020-04-15 19:56:25 +0600

    updated celsius and time

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var request  = require('request');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 
-var city = 'Tangail';
+var city = 'Dhaka';
+app.use(bodyParser.json());
 
 
 
@@ -44,14 +45,23 @@ app.post('/weather', (req,res)=>{
     res.redirect('/weather2/'+city);
 })
 
+app.post('/weather2', (req,res)=>{ 
+    var city = req.body.city;
+    res.redirect('/weather2/'+ city);
+})
+
 app.get('/weather2/:city', function (req, res) {
     var city  = req.params.city;
+    
+    
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
 
     request(url, function (error, respone, body) {
         var weather_json = JSON.parse(body);
-        console.log(weather_json);
+        //console.log(weather_json);
         var datetime = new Date().toDateString();
+        var cel = ((Math.round(weather_json.main.temp)- 32) * 5)/9;
+        
         
         if (!weather_json.message)
         {
@@ -72,6 +82,58 @@ app.get('/weather2/:city', function (req, res) {
             var data = { data: data }
             res.render('weather2.ejs', data);
         }else{
+            res.send('City not found');
+        }
+
+
+    });
+
+});
+
+
+app.post('/cel', function (req, res) {
+     var city = req.body.city;
+     
+     res.redirect('/cel/'+ city)    
+     
+});
+
+
+app.get('/cel/:city', function (req, res) {
+    var city = req.params.city;
+    //console.log(city);
+    
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
+
+    request(url, function (error, respone, body) {
+        var weather_json = JSON.parse(body);
+        //console.log(weather_json);
+        var datetime = new Date().toDateString();
+        var cel = ((Math.round(weather_json.main.temp) - 32) * 5) / 9;
+        var max = ((Math.round(weather_json.main.temp_max) - 32) * 5) / 9;
+        var km = ((weather_json.wind.speed)*1.6093440)
+      var kmp =  km.toFixed(3);
+
+
+
+        if (!weather_json.message) {
+
+            var data = {
+                city: weather_json.name,
+                temp: Math.round(cel),
+                icon: weather_json.weather[0].icon,
+                des: weather_json.weather[0].description,
+                humi: weather_json.main.humidity,
+                country: weather_json.sys.country,
+                wind: kmp,
+                temp_max: Math.round(max),
+                pressure: weather_json.main.pressure,
+                date: datetime
+
+            }
+            var data = { data: data }
+            res.render('cel.ejs', data);
+        } else {
             res.send('City not found');
         }
 
